fc99b6deb786fd679804c4c0f9626a4936fd3163
Sweepy86 <stengel.alexander@yahoo.de>
2015-06-25 11:58:43 +0200

    change client
    don’t work

--- Dienstnutzer/client.js
+++ Dienstnutzer/client.js
@@ -1,26 +1,20 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var app = express();
-
-var context = require('rabbit.js').createContext('amqp://localhost');
-var pub = context.socket('PUBLISH');
+var context = require('rabbit.js').createContext('amqp://localhost:3000');
 
 context.on('ready', function() {
-  var pub = sub = context.socket('SUB');
+  var express = require('express');
+  var http = require('http');
+  var app = express();
+  var sub = context.socket('SUB');
+  var pub = context.socket('PUB'); 
   sub.pipe(process.stdout);
   sub.connect('events', function() {
       sub.setEncoding('utf8');
        sub.on('data', function(note) { console.log("Alarum! %s", note); })
         sub.end();
     });
+    console.log('Client is running, listening on Port 5000.')
+app.listen(5000);
   });
-
-
-app.get('/', function(req,res){
-    res.sendStatus(200, 'OK');
-    console.log('Client erreichbar.');
-});
 
 function user(){
 }
@@ -36,5 +30,3 @@ function bookreturn(){
 
 
 
- console.log('Client is running, listening on Port 5000.')
-app.listen(5000);
